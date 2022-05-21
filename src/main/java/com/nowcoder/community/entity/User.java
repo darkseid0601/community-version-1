22994cc7f6ce6f5b94d5747b64dd120486aa9108
DarkSeid0601 <1378776476@qq.com>
2022-05-21 21:34:20 +0800

    2022-5-21

--- src/main/java/com/nowcoder/community/entity/User.java
+++ src/main/java/com/nowcoder/community/entity/User.java
@@ -1,6 +1,7 @@
 package com.nowcoder.community.entity;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import java.util.Date;
@@ -13,6 +14,7 @@ import java.util.Date;
  */
 @Data
 @ToString
+@NoArgsConstructor
 public class User {
     private int id;
     private String username;
